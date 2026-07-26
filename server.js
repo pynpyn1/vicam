9be14e8ad40314bf4b368a2d0bd70ba6c97955d0
actzy <rahmadalvin54@gmail.com>
2026-07-26 10:47:15 +0700

    Update port to 3003

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
